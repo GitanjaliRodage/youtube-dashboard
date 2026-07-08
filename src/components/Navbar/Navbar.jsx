891ba6c309baf2e9bbe866625134a0e87d27f5e8
Gitanjali Rodage <gitanjali.rodage.aids.2023@vpkbiet.org>
2026-07-08 10:39:52 +0530

    Update YouTube dashboard UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
 import "./Navbar.css";
+import { MdMenu, MdMic } from "react-icons/md";
+import {
+  FaYoutube,
+  FaSearch,
+  FaVideo,
+  FaBell,
+  FaUserCircle,
+} from "react-icons/fa";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">
-        <h2>YouTube Dashboard</h2>
+      <div className="nav-left">
+        <MdMenu className="menu-icon" />
+        <FaYoutube className="youtube-icon" />
+        <h2>YouTube</h2>
+      </div>
+
+      <div className="nav-center">
+        <input type="text" placeholder="Search" />
+        <button className="search-btn">
+          <FaSearch />
+        </button>
+        <button className="mic-btn">
+          <MdMic />
+        </button>
+      </div>
+
+      <div className="nav-right">
+        <FaVideo />
+        <FaBell />
+        <FaUserCircle />
       </div>
     </nav>
   );
